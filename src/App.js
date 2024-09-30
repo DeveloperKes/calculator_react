6590462d3c61cdc5567c74cb6f861269a11e0a5c
DeveloperKes <kevin.sanchez8117@gmail.com>
2024-09-30 16:08:13 -0500

    Calculator first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("0");
+  const [record, setRecordValue] = useState("");
+  const [operator, setOperator] = useState("+");
+
+  const handleSetNumber = (number) => {
+    setValue(`${value === '0' ? '' : value}${number}`);
+  }
+
+  const handleReset = (clear = false) => {
+    if (clear) setRecordValue("");
+    setValue("0");
+  };
+
+  const handleBackspace = () => {
+    const newValue = value.slice(0, -1);
+    setValue(newValue === '' ? '0' : newValue);
+  }
+
+  const handleOperation = (operatorSymbol) => {
+    if (record) handleEqual();
+    else setRecordValue(`${value} ${operatorSymbol}`);
+    setOperator(operatorSymbol);
+    setValue("0");
+  }
+
+  const handleEqual = (final = false) => {
+    const dicOperations = {
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "x": (a, b) => a * b,
+      "÷": (a, b) => b !== 0 ? a / b : "Error"
+    }
+
+    const currentNumber = parseFloat(value.replace(',', '.'));
+    const previousNumber = parseFloat(record.split(' ')[0].replace(',', '.'));
+
+    console.log(currentNumber, previousNumber);
+
+
+    const result = dicOperations[operator](previousNumber, currentNumber);
+    if (final) {
+      if (result === 'Error') setValue(result);
+      else setValue(`${result.toString().replace('.', ',')}`);
+      setRecordValue("");
+    } else {
+      if (result === 'Error') setRecordValue(result);
+      else setRecordValue(`${result.toString().replace('.', ',')} ${operator}`);
+    }
+  }
+
+  const handlePercent = () => {
+    if (value > 0) {
+      const percent = value / 100;
+      setValue(percent);
+    }
+  }
+
+  const handleDecimal = () => {
+    if (!value.includes(',')) {
+      const newValue = `${value},`;
+      setValue(newValue);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="calculator">
+      <section className="screen">
+        <label>{record}</label>
+        <input type="text" style={{
+          fontSize: value.length < 8 ? '3rem' : value.length >= 14 ? `${3 - (14 / 10)}rem` : `${3 - (value.length / 10)}rem`,
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          width: '100%',
+        }} value={value.length > 14 ? value.slice(0, 14) + '...' : value} readOnly />
+      </section>
+      <section className="commands">
+        <button onClick={handlePercent} className="calc_button">%</button>
+        <button onClick={() => handleReset(true)} className="calc_button">CE</button>
+        {/* <button onClick={handleReset} className="calc_button">C</button> */}
+        <button onClick={handleBackspace} className="calc_button operator">🆑</button>
+      </section>
+      <section className="second_command"></section>
+      <section className="numbers">
+        {
+          [1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+            <button key={`button-${number}`} onClick={() => handleSetNumber(number)} className="calc_button number">{number}</button>
+          ))
+        }
+        <button onClick={() => handleSetNumber(0)} className="calc_button number zero">0</button>
+        <button onClick={handleDecimal} className="calc_button number">,</button>
+      </section>
+      <aside className="operators">
+        <button onClick={() => handleOperation("÷")} className="calc_button operator">÷</button>
+        <button onClick={() => handleOperation("x")} className="calc_button operator">×</button>
+        <button onClick={() => handleOperation("-")} className="calc_button operator">–</button>
+        <button onClick={() => handleOperation("+")} className="calc_button operator">+</button>
+        <button onClick={() => handleEqual(true)} className="calc_button operator">=</button>
+      </aside>
+    </main>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,70 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const screenElement = screen.getByText(/%/i);
+  expect(screenElement).toBeInTheDocument();
 });
+
+test('initial value is 0', () => {
+  render(<App />);
+  const display = screen.getByDisplayValue("0");
+  expect(display).toBeInTheDocument();
+});
+
+test('set number 5', () => {
+  render(<App />);
+  const button5 = screen.getByText("5");
+  fireEvent.click(button5);
+  const display = screen.getByDisplayValue("5");
+  expect(display).toBeInTheDocument();
+})
+
+test('perform addition 2 + 3 = 5', () => {
+  render(<App />);
+
+  const button2 = screen.getByText("2");
+  fireEvent.click(button2);
+
+  const addButton = screen.getByText("+");
+  fireEvent.click(addButton);
+
+  const button3 = screen.getByText("3");
+  fireEvent.click(button3);
+
+  const equalsButton = screen.getByText("=");
+  fireEvent.click(equalsButton);
+
+  const display = screen.getByDisplayValue("5");
+  expect(display).toBeInTheDocument();
+})
+
+test('division by 0 returns error', () => {
+  render(<App />);
+
+  const button2 = screen.getByText("2");
+  fireEvent.click(button2);
+
+  const divideButton = screen.getByText("÷");
+  fireEvent.click(divideButton);
+
+  const equalButton = screen.getByText("=");
+  fireEvent.click(equalButton);
+
+  const display = screen.getByDisplayValue("Error");
+  expect(display).toBeInTheDocument();
+})
+
+test('reset calculator', () => {
+  render(<App />);
+
+  const button8 = screen.getByText("8");
+  fireEvent.click(button8);
+
+  const clearButton = screen.getByText("CE");
+  fireEvent.click(clearButton);
+
+  const display = screen.getByDisplayValue("0");
+  expect(display).toBeInTheDocument();
+})
